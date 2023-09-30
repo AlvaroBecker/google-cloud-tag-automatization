26a77f15fed2a178b7f80f613e5e9d433942c80c
Alvaro becker <alvarobeckerruiz@gmail.com>
2023-09-29 21:00:39 -0300

    refactoring

--- main.py
+++ main.py
@@ -1,46 +1,49 @@
-from google.cloud import datacatalog_v1
-import os
-from google.oauth2.service_account import Credentials
-from google.cloud import bigquery
-import funciones
-import time
 import json
+import os
+import time
 
-# Set the path to the JSON file containing the service account key
+from google.cloud import bigquery, datacatalog_v1
+from google.oauth2.service_account import Credentials
 
-path_to_credentials =os.environ["GOOGLE_APPLICATION_CREDENTIALS"] 
-# Load the service account key
-credentials = Credentials.from_service_account_file(path_to_credentials)
-dclient = datacatalog_v1.DataCatalogClient(credentials=credentials)
-client = bigquery.Client()
-# Set the project_id, tag_template, and location variables
-project_id = os.environ["PROJECT_ID"]
-tag_template = os.environ["TAG_TEMPLATE"]
-location = os.environ["LOCATION"]
-template_name = f"projects/{project_id}/locations/{location}/tagTemplates/{tag_template}"
+import funciones
 
-diccionario = os.environ["DICCIONARIO"]
-
-start_time = time.time()
-
-check={}
-"""
-TO TEST THIS PROJECT YOU NEED TO DELETE THE TAGS FROM THE TABLES 
-AND DATASETS YOU WANT TO USE FOR TESTING, YOU CAN ADD A DICCTIONARY ON THE ENVIRONMENT VARIABLES
-OR CREATE A JSON FILE AND LOAD IT ON THE MAIN.PY FILE
-"""
+# Constants
+PATH_TO_CREDENTIALS = os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+PROJECT_ID = os.environ["PROJECT_ID"]
+TAG_TEMPLATE = os.environ["TAG_TEMPLATE"]
+LOCATION = os.environ["LOCATION"]
+TEMPLATE_NAME = f"projects/{PROJECT_ID}/locations/{LOCATION}/tagTemplates/{TAG_TEMPLATE}"
+DICCIONARIO = os.environ["DICCIONARIO"]
 
 
-"""
-#Open the json file and load it as a dictionary and comment the first "diccionario" variable
-with open('dictionary.json', 'r') as file:
-    diccionario = json.load(file)
-"""
-datasets_tables=funciones.get_datasets_list(client)
-tag_fields_list=funciones.get_templates_fields(dclient, template_name)
+def main():
+    """Main function."""
+    start_time = time.time()
 
-funciones.tag_table(project_id, dclient, location, tag_template, tag_fields_list, datasets_tables, diccionario, template_name,client)
+    # Load the service account key
+    credentials = Credentials.from_service_account_file(PATH_TO_CREDENTIALS)
+    dclient = datacatalog_v1.DataCatalogClient(credentials=credentials)
+    client = bigquery.Client()
 
-end_time = time.time()
-execution_time = end_time - start_time
-print(execution_time)       
+    """
+    TO TEST THIS PROJECT YOU NEED TO DELETE THE TAGS FROM THE TABLES 
+    AND DATASETS YOU WANT TO USE FOR TESTING, YOU CAN ADD A DICTIONARY ON THE ENVIRONMENT VARIABLES
+    OR CREATE A JSON FILE AND LOAD IT ON THE MAIN.PY FILE
+    """
+
+    # Uncomment to load the dictionary from a JSON file
+    # with open('dictionary.json', 'r') as file:
+    #     diccionario = json.load(file)
+
+    datasets_tables = funciones.get_datasets_list(client)
+    tag_fields_list = funciones.get_templates_fields(dclient, TEMPLATE_NAME)
+
+    funciones.tag_table(PROJECT_ID, dclient, LOCATION, TAG_TEMPLATE, tag_fields_list, datasets_tables, DICCIONARIO, TEMPLATE_NAME, client)
+
+    end_time = time.time()
+    execution_time = end_time - start_time
+    print(f"Execution time: {execution_time:.2f} seconds")
+
+
+if __name__ == "__main__":
+    main()
